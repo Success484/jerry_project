8b2198e7a6196f7eb0e8ff84f02465e29ccc6fec
Success <successsimeon484@gmail.com>
2025-02-11 19:10:53 +0100

    feat: cong_DB

--- jdi/settings.py
+++ jdi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jdi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lpffTcHXWZRrhbxiKzoGcVMLBLYpYJIE',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '18506',
     }
 }
 
